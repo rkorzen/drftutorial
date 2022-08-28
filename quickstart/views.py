81e77f3e5fddb8a4d9e3597850b45f2864b4a0a1
rkorzen <korzeniewski@gmail.com>
2022-08-28 12:14:18 +0200

    quick example

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from rest_framework import viewsets
+from rest_framework import permissions
+from quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoints that allows users to be viewed or edited
+    """
+
+    queryset = User.objects.order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoints that allows groups to be viewed or edited
+    """
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o)@fhjqg-zke=0bjql#ad$n+#=@1mvxg(l6q*-j%dr_4u!gkh="
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-o)@fhjqg-zke=0bjql#ad$n+#=@1mvxg(l6q*-j%dr_4u!gkh=",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,3 +126,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
